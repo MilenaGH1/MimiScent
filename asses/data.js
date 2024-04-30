d0266da5b6cddf89fa6dd5e611859320888a3339
MilenaGarcia <you@example.com>
2024-04-30 19:43:15 -0300

    se crean documentos js y se crea array de objetos

---
+++ asses/data.js
@@ -0,0 +1,25 @@
+const productsData = [
+    {
+        id: 1,
+        name:Lancome,
+        description:"La vie est belle EDP refillable",
+        img : "./Image/lancome.jpeg",
+        precio: 58.500 ,
+    },
+
+    {
+        id: 2,
+        name:Versace,
+        description:"Coleccion Diamond EDP",
+        img : "./Image/Versace.jpeg",
+        precio: 136.500 ,
+    },
+
+    {
+        id: 3,
+        name:HUGO-BOSS ,
+        description:"Boss Bottle United EDT",
+        img : "../Image/Boss-hombre.jpg",
+        precio: 39.500,
+    },
+]
